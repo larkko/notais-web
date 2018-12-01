33e67979874c7605f488fd2344b0411ac2661cb7
Lasse Arkko <lasse.arkko@gmail.com>
2018-12-01 04:59:03 +0200

    Add root component for app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 console.log('Notais v. 0.0.1');
+
+const App = () =>
+  <div>Notais v. 0.0.1</div>;
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
+
+
+
+
+
+
