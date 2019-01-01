bcbf337b443a3553f4d4147d18483730e1a5c736
Lasse Arkko <lasse.arkko@gmail.com>
2019-01-01 20:57:12 +0200

    Update src/index.js to utilize recent additions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import {
+  connect,
+  Provider
+} from 'react-redux';
+import rootReducer from './reducers';
+import {
+  PressKeyboardKey,
+  pressKeyboardKey,
+  ReleaseKeyboardKey,
+  releaseKeyboardKey
+} from './actions/keyboard';
+import Instrument from './views/instrument';
 
 console.log('Notais v. 0.0.1');
 
-const App = () =>
-  <div>Notais v. 0.0.1</div>;
+const store = createStore(rootReducer);
 
-ReactDOM.render(<App/>, document.getElementById("root"));
+const App = props => {
+  return (
+    <Instrument {...props}/>
+  );
+};
+
+const mapStateToProps = state => {
+  return {...state};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    pressKeyboardKey: ({index, velocity}) => {
+      return dispatch(pressKeyboardKey({index, velocity}))
+    },
+    releaseKeyboardKey: index => dispatch(releaseKeyboardKey(index))
+  };
+};
+
+const ConnectedApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedApp/>
+  </Provider>,
+  document.getElementById("root")
+);
 
 
 
