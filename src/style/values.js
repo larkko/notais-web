c67f58ee23b8a7ea04a2605bf0f13f00461d9ea6
Lasse Arkko <lasse.arkko@gmail.com>
2019-01-01 20:52:29 +0200

    Add style values file
    This file keeps frequently used style values in one place.

---
+++ src/style/values.js
@@ -0,0 +1,19 @@
+
+export const activeColor = '#0DDDDD';
+export const secondaryBackgroundColor = '#333333';
+export const borderColor = 'white';
+export const borderRadius = '1em';
+export const borderWidth = 'medium';
+
+
+
+
+
+
+
+
+
+
+
+
+
