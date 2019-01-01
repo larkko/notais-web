893c13b59301e8216c82c0e64abac60b1fb16a4a
Lasse Arkko <lasse.arkko@gmail.com>
2019-01-01 20:51:34 +0200

    Add root reducer
    This is the reducer that will be passed onto the root component of the application.

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+import keyboard from './keyboard';
+
+const rootReducer = combineReducers({
+  keyboard
+});
+
+export default rootReducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
