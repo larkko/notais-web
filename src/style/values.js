80e141ede92aeef74628df189d73db6036f4cc7f
Lasse Arkko <lasse.arkko@gmail.com>
2019-01-07 03:59:58 +0200

    Use a more appropriate color set

--- src/style/values.js
+++ src/style/values.js
@@ -1,9 +1,10 @@
 
 export const activeColor = '#0DDDDD';
+export const backgroundColor = '#E6E6E6';
 export const secondaryBackgroundColor = '#333333';
-export const borderColor = 'white';
+export const borderColor = 'black';
 export const borderRadius = '1em';
-export const borderWidth = 'medium';
+export const borderWidth = 'thick';
 
 
 
